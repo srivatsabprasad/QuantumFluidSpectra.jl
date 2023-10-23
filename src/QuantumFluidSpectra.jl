6b22e5818fa9b11f958aba4370099edb81a2b655
Srivatsa B. Prasad <69298943+srivatsabprasad@users.noreply.github.com>
2023-10-23 19:03:57 +0100

    sinc_reduce_alt, incompressible_spectrum_alt

--- src/QuantumFluidSpectra.jl
+++ src/QuantumFluidSpectra.jl
@@ -6,6 +6,7 @@ using SpecialFunctions
 using PaddedViews
 using UnPack
 using TensorCast
+using ThreadsX
 
 # fallback since fast_hypot is 2 argument only
 @fastmath hypot(x::Float64, y::Float64, z::Float64)=sqrt(x^2+y^2+z^2)
@@ -45,13 +46,13 @@ include("analysis.jl")
 
 export Psi, Psi_real, Psi_qper2, Psi_qper3, xvecs, kvecs
 export auto_correlate, cross_correlate
-export bessel_reduce, sinc_reduce, gv, gv3
+export bessel_reduce, sinc_reduce, gv, gv3, sinc_reduce_alt
 export log10range, convolve
 
 export xk_arrays, xk_arraysalt, fft_differentials
 export gradient, gradient_qper, velocity, velocity_qper, current, current_qper
 export energydecomp, energydecomp_qper, helmholtz, kinetic_density, kinetic_density_qper, full_spectrum, full_spectrum_qper, full_current_spectrum, full_current_spectrum_qper
-export incompressible_spectrum, incompressible_spectrum_qper, compressible_spectrum, compressible_spectrum_qper, qpressure_spectrum
+export incompressible_spectrum, incompressible_spectrum_qper, compressible_spectrum, compressible_spectrum_qper, qpressure_spectrum, incompressible_spectrum_alt
 export incompressible_current_spectrum, incompressible_current_spectrum_qper, compressible_current_spectrum, compressible_current_spectrum_qper
 export incompressible_density, incompressible_density_qper, compressible_density, compressible_density_qper, qpressure_density
 export ic_density, ic_density_qper, iq_density, iq_density_qper, cq_density, cq_density_qper
