10e88f5308f7f2c63bfb30e7929d83c634a8a9ef
Srivatsa B. Prasad <69298943+srivatsabprasad@users.noreply.github.com>
2023-10-24 00:30:54 +0100

    add fft_planner

--- src/QuantumFluidSpectra.jl
+++ src/QuantumFluidSpectra.jl
@@ -50,7 +50,7 @@ export auto_correlate, cross_correlate
 export bessel_reduce, sinc_reduce, gv, gv3, sinc_reduce_alt
 export log10range, convolve
 
-export xk_arrays, xk_arrays0, fft_differentials
+export xk_arrays, xk_arrays0, fft_differentials, fft_planner
 export gradient, gradient_qper, velocity, velocity_qper, current, current_qper
 export energydecomp, energydecomp_qper, helmholtz, kinetic_density, kinetic_density_qper, full_spectrum, full_spectrum_qper, full_current_spectrum, full_current_spectrum_qper
 export incompressible_spectrum, incompressible_spectrum_qper, compressible_spectrum, compressible_spectrum_qper, qpressure_spectrum, incompressible_spectrum_alt
