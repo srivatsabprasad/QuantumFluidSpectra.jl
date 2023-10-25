5bd7c6f5864f9fe9a903340ecad9d424b753d502
Srivatsa B. Prasad <69298943+srivatsabprasad@users.noreply.github.com>
2023-10-25 22:52:25 +0100

    decomposed_spectra, decomposed_current_spectra

--- src/QuantumFluidSpectra.jl
+++ src/QuantumFluidSpectra.jl
@@ -43,14 +43,14 @@ include("analysis.jl")
 
 export Psi, Psi_qper2, Psi_qper3, xvecs, kvecs
 export auto_correlate, cross_correlate
-export bessel_reduce, sinc_reduce, gv, gv3, sinc_reduce_alt
+export bessel_reduce, sinc_reduce, sinc_reduce_real, sinc_reduce_complex, gv, gv3, sinc_reduce_alt
 export log10range, convolve
 
 export xk_arrays, xk_arrays0, fft_differentials, correlation_measure, fft_planner
 export gradient, gradient_qper, velocity, velocity_qper, weightedvelocity, weightedvelocity_qper, current, current_qper
-export energydecomp, energydecomp_qper, helmholtz, kinetic_density, kinetic_density_qper, full_spectrum, full_spectrum_qper, full_current_spectrum, full_current_spectrum_qper
-export incompressible_spectrum, incompressible_spectrum_qper, compressible_spectrum, compressible_spectrum_qper, qpressure_spectrum, incompressible_spectrum_alt
-export incompressible_current_spectrum, incompressible_current_spectrum_qper, compressible_current_spectrum, compressible_current_spectrum_qper
+export energydecomp, energydecomp_qper, helmholtz, kinetic_density, full_spectrum, full_current_spectrum
+export incompressible_spectrum, compressible_spectrum, decomposed_spectra, qpressure_spectrum
+export incompressible_current_spectrum, compressible_current_spectrum, decomposed_current_spectra
 export incompressible_density, incompressible_density_qper, compressible_density, compressible_density_qper, qpressure_density
 export ic_density, ic_density_qper, iq_density, iq_density_qper, cq_density, cq_density_qper
 export density_spectrum, trap_spectrum
