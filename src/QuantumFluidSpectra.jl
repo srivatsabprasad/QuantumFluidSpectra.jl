73b297fd3078dc6179538c44f57dd8229a0bd349
Srivatsa B. Prasad <69298943+srivatsabprasad@users.noreply.github.com>
2024-11-07 23:34:10 +0000

    Update QuantumFluidSpectra.jl
    Remove FFTW Threads specification

--- src/QuantumFluidSpectra.jl
+++ src/QuantumFluidSpectra.jl
@@ -10,8 +10,6 @@ using UnPack
 using ThreadsX
 using Referenceables
 
-FFTW.set_num_threads(Threads.nthreads())
-
 # fallback since fast_hypot is 2 argument only
 @fastmath hypot(x::Float64, y::Float64, z::Float64)=sqrt(x^2+y^2+z^2)
 export hypot 
